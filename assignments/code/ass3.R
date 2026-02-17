d143b8d3b0b16b4a5b8e9177c7889092e75ca18f
Fran Villamil <franvillamil@gmail.com>
2026-02-17 13:35:06 +0100

    autoupdate

--- assignments/code/ass3.R
+++ assignments/code/ass3.R
@@ -1,1 +1,12 @@
 setwd("~/Downloads")
+library(dplyr)
+
+raw = read.csv("https://raw.githubusercontent.com/franvillamil/AQM2/refs/heads/master/datasets/anes/anes_timeseries_2020.csv")
+
+df = raw %>%
+    select(voted,
+        age,
+        female,
+        education,
+        income,
+        party_id)
